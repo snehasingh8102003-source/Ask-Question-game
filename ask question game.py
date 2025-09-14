d4c63dc5d8416308930a0863d8cf74c202ed65d0
Sneha Singh <snehasingh8102003@gmail.com>
2025-09-14 17:38:16 +0530

    Add files via upload

---
+++ ask question game.py
@@ -0,0 +1,35 @@
+# Answer the questions. 
+n=input("Do you want to play game" )
+if(n!="yes"):
+    quit()
+print("Let's play")
+q1=input("what is our national flag? ")
+if(q1=="tiranga"):
+    print("right")
+else:
+    print("wrong")
+q2=input("what is our national game? ")
+if(q2=="hockey"):
+    print("right")
+else:
+    print("wrong")
+q3=input("what is our national bird? ")
+if(q3=="peacock"):
+    print("right")
+else:
+    print("wrong")
+q3=input("what is our national anthem? ")
+if(q3=="jana gana mana"):
+    print("right")
+else:
+    print("wrong")
+q4=input("what is our national song? ")
+if(q4=="vande mataram"):
+    print("right")
+else:
+    print("wrong")
+q5=input("who is our first prime minister? ")
+if(q5=="Pt. Jawaharlal nehru"):
+    print("right")
+else:
+    print("wrong")
